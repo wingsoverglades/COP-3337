d5971c709447689069cf7ce4609164ea5fc7484a
luisv <wingsoverglades@gmail.com>
2017-10-09 11:52:27 -0400

    Added assignment 3 classes and some info in headers

---
+++ src/Assignment 3 - Craps/Analyzer.java
@@ -0,0 +1,54 @@
+/*==============================================================================  
+|   Source code:    Analyzer.java
+|   Author:         Luis Vitier
+|   Student ID:     5723008 
+|   Assignment:     Program #[n] [Title]
+|  
+|   Course:     COP 3337 (Programming 2)
+|   Section:    U09 
+|   Instructor: William Feild  
+|   Due Date:   October 12, 2017, at the beginning of class
+|
+|	I hereby certify that this collective work is my own and none of it is the
+|   work of any other person or entity.
+|	______________________________________ [Signature]
+|  
+|   Language:       Java 8
+|   Compile/Run:
+| 	javac Analyzer.java Craps.java Die.java
+|   java Analyzer
+| 
++-------------------------------------------------------------------------------  
+|  
+|   Description:    [Describe the program's goal, IN DETAIL.]  
+|                  
+|   Input:          [Describe the sources and formats of all user-supplied input
+|                   required by the program.]  
+|  
+|   Output:         [Describe the output that the user should expect the program
+|                   to produce.]  
+|  
+|   Process:        [Names of standard algorithms employed, explanations of why
+|                   things were done they way they were done, etc. This is the
+|                   place for technical information that another programmer
+|                   would like to know.]  
+|  
+|   Required Features Not Included:  
+|                   [If the assignment specifies a feature that you were unable
+|                   to include in the program, mention that omission here.
+|                   Otherwise, state that you were able to include all of the
+|                   required features in your program. Being honest here may
+|                   save you some points.]  
+|  
+|   Known Bugs:     [If you know of any problems with the code, provide details
+|                   here; otherwise, clearly state that you know of no logic
+|                   errors. Being honest here may save you some points.]  
+*=============================================================================*/
+
+public class Analyzer
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/Assignment 3 - Craps/Die.java
@@ -0,0 +1,58 @@
+/*==============================================================================
+|   Source code:    Die.java
+|   Class:          Die
+|
+|   Author:     Luis Vitier
+|   Student ID: 5723008
+|   Assignment: Program #3 - Craps
+|  
+|   Course:     COP 3337 (Intermediate Programming)
+|   Section:    U09
+|   Instructor: William Feild  
+|   Due Date:   12 October, 2017, by the beginning of class.
+|
+|	I hereby certify that this collective work is my own and none of it is the
+|   work of any other person or entity.
+|	______________________________________ [Signature]
+|  
+|   Language:  Java
+|   Compile/Run: 
+| 	javac Analyzer.java Craps.java Die.java
+|	java Analyzer
+|
+|   Purpose:        This class exists to simulate the behavior of a single Die.
+|                   It is general purpose.
+|
+|   Inherits From:  the "Object" class
+|
+|   Interfaces:     None
+|
++-------------------------------------------------------------------------------
+|
+|   Constants:  [Name all public class constants, and provide a very
+|                   brief (but useful!) description of each.]
+|
++-------------------------------------------------------------------------------
+|
+|   Constructors:   Public constructor Die, takes no arguments.
+|
+|   Class Methods:  [List the names, arguments, and return types of all
+|                   private class methods.]
+|
+|   Instance Methods:  [List the names, arguments, and return types of all
+|                   public instance methods.]
+|
+*=============================================================================*/
+
+public class Die
+{
+    private int face = 1;
+
+    public Die()
+    {}
+
+    public rollDie()
+    {
+
+    }
+}
