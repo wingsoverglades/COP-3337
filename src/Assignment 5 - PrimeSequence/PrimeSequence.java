1c657e34acb1443503d7cdfbe0e48b31a2431d7d
luisv <wingsoverglades@gmail.com>
2017-11-01 11:13:32 -0400

    Added source files

---
+++ src/Assignment 5 - PrimeSequence/PrimeSequence.java
@@ -0,0 +1,53 @@
+/*==============================================================================
+|   Source code:    PrimeSequence.java
+|   Class:          PrimeSequence
+|
+|   Author:     Luis Vitier
+|   Student ID: 5723008
+|   Assignment: Program #n - name
+|  
+|   Course:     COP 3337 (Intermediate Programming)
+|   Section:    U09
+|   Instructor: William Feild  
+|   Due Date:   1 January 2000, by the end of class 
+|
+|	I hereby certify that this collective work is my own and none of it is the
+|   work of any other person or entity.
+|	______________________________________ [Signature]
+|  
+|   Language:  Java
+|   Compile/Run: 
+| 	javac PrimeSequence.java
+|	java PrimeSequence
+|
+|   Purpose:  [A description of why this class exists.  For what
+|                   reason was it written?  Which jobs does it perform?]
+|
+|   Inherits From:  [If this class is a subclass of another, name it.
+|                   If not, just say "None."]
+|
+|   Interfaces:  [If any predefined interfaces are implemented by
+|                  this class, name them.  If not, ... well, you know.]
+|
++-------------------------------------------------------------------------------
+|
+|   Constants:  [Name all public class constants, and provide a very
+|                   brief (but useful!) description of each.]
+|
++-------------------------------------------------------------------------------
+|
+|   Constructors:  [List the names and arguments of all defined
+|                   constructors.]
+|
+|   Class Methods:  [List the names, arguments, and return types of all
+|                   private class methods.]
+|
+|   Instance Methods:  [List the names, arguments, and return types of all
+|                   public instance methods.]
+|
+*=============================================================================*/
+
+public class PrimeSequence
+{
+
+}
