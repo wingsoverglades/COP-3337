33bc98eb2975ffac54212a30be56816b4709ca82
luisv <wingsoverglades@gmail.com>
2017-11-01 12:28:23 -0400

    Finished interface

--- src/Assignment 5 - PrimeSequence/Sequence.java
+++ src/Assignment 5 - PrimeSequence/Sequence.java
@@ -4,12 +4,12 @@
 |
 |   Author:     Luis Vitier
 |   Student ID: 5723008
-|   Assignment: Program #n - name
+|   Assignment: Program #5 - Sequence of Primes
 |  
 |   Course:     COP 3337 (Intermediate Programming)
 |   Section:    U09
 |   Instructor: William Feild  
-|   Due Date:   1 January 2000, by the end of class 
+|   Due Date:   November 9, 2017, by the end of class
 |
 |	I hereby certify that this collective work is my own and none of it is the
 |   work of any other person or entity.
@@ -17,37 +17,38 @@
 |  
 |   Language:  Java
 |   Compile/Run: 
-| 	javac Sequence.java
-|	java Sequence
+| 	javac SequenceDemo.java Sequence.java PrimeSequence.java
+|	java SequenceDemo
 |
-|   Purpose:  [A description of why this class exists.  For what
-|                   reason was it written?  Which jobs does it perform?]
+|   Purpose:        This interface exists to provide the template for the
+|                   PrimeSequence class.
 |
-|   Inherits From:  [If this class is a subclass of another, name it.
-|                   If not, just say "None."]
+|   Inherits From:  None
 |
-|   Interfaces:  [If any predefined interfaces are implemented by
-|                  this class, name them.  If not, ... well, you know.]
+|   Interfaces:     None
 |
 +-------------------------------------------------------------------------------
 |
-|   Constants:  [Name all public class constants, and provide a very
-|                   brief (but useful!) description of each.]
+|   Constants:  No constants
 |
 +-------------------------------------------------------------------------------
 |
-|   Constructors:  [List the names and arguments of all defined
-|                   constructors.]
+|   Constructors:   Class has no constructors
 |
-|   Class Methods:  [List the names, arguments, and return types of all
-|                   private class methods.]
+|   Class Methods:  No class methods
 |
-|   Instance Methods:  [List the names, arguments, and return types of all
-|                   public instance methods.]
+|   Instance Methods:
+|                   long next() takes one long parameter.
 |
 *=============================================================================*/
 
 public interface Sequence
 {
-
+    /**
+     * This interface method finds the next element in the sequence.
+     *
+     * @param number    Find the next element after this value.
+     * @return          The value of the next element.
+     */
+    long next(long number);
 }
